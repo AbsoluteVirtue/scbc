d54b0b8855632971c336548dcfdc179acc98b11b
AbsoluteVirtue <3hitemouse@gmail.com>
2023-03-06 16:49:04 +0200

    tweaked js

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Visualization of barterable Stalcraft items for easy comparison and calculations.
 
 Original [developer's documentation](https://eapi.stalcraft.net/items.html).
+
+Numerical data [source](https://docs.google.com/spreadsheets/d/1XGOHPJwlgGqj3KL12kQNNu3qv6iy_cLC_wFNN76B9Mo/).
---
+++ docs/db.js
@@ -0,0 +1,558 @@
+const data = {
+    "armor": [
+        {
+            "exboid": "ly3kj",
+            "id": "apo_a",
+            "class": "combat",
+            "lv": "master",
+            "bonus": {
+                "spe": -20,
+                "end": -40,
+                "nvg": 1
+            },
+            "stats": {
+                "pul": 256,
+                "raz": 300,
+                "vzr": 300,
+                "ele": 53,
+                "ogo": 53,
+                "him": 53,
+                "rad": 200,
+                "tem": 50,
+                "bio": 200,
+                "psi": 170,
+                "kro": 0
+            },
+            "cost": {
+                "rub": 4141130,
+                "may": 760,
+                "pap": 2380,
+                "ves": 4780,
+                "dig": 1490,
+                "qua": 2140,
+                "syv": 90
+            }
+        },
+        {
+            "exboid": "1rkq2",
+            "id": "ska_a",
+            "class": "research",
+            "lv": "veteran",
+            "bonus": {
+                "end": -6,
+                "wei": 10,
+                "nvg": 1
+            },
+            "stats": {
+                "pul": 118,
+                "raz": 127,
+                "vzr": 171,
+                "ele": 296,
+                "ogo": 296,
+                "him": 296,
+                "rad": 200,
+                "tem": 120,
+                "bio": 210,
+                "psi": 170,
+                "kro": 56
+            },
+            "cost": {
+                "rub": 694030,
+                "may": 640,
+                "dig": 320,
+                "pap": 228,
+                "ves": 1610
+            }
+        },
+        {
+            "exboid": "5lrg1",
+            "id": "pah_b",
+            "class": "research",
+            "lv": "veteran",
+            "stats": {
+                "pul": 106,
+                "raz": 115,
+                "vzr": 154,
+                "ele": 267,
+                "ogo": 267,
+                "him": 267,
+                "rad": 200,
+                "tem": 90,
+                "bio": 200,
+                "psi": 160,
+                "kro": 53
+            },
+            "cost": {
+                "rub": 604630,
+                "may": 275,
+                "pap": 216,
+                "ves": 650
+            },
+            "bonus": {
+                "end": -4,
+                "wei": 10,
+                "nvg": 1
+            }
+        },
+        {
+            "exboid": "zzjk2",
+            "id": "tuz_b",
+            "class": "research",
+            "lv": "master",
+            "stats": {
+                "pul": 149,
+                "raz": 160,
+                "vzr": 229,
+                "ele": 107,
+                "ogo": 107,
+                "him": 107,
+                "rad": 200,
+                "tem": 80,
+                "bio": 200,
+                "psi": 200,
+                "kro": 65
+            },
+            "cost": {
+                "rub": 2201030,
+                "may": 820,
+                "pap": 1280,
+                "ves": 3840,
+                "dig": 600,
+                "qua": 860,
+                "syv": 75
+            },
+            "bonus": {
+                "end": -9,
+                "wei": 45,
+                "nvg": 1
+            }
+        },
+        {
+            "exboid": "qj1y6",
+            "id": "tuz_a",
+            "class": "research",
+            "lv": "master",
+            "stats": {
+                "pul": 137,
+                "raz": 147,
+                "vzr": 210,
+                "ele": 98,
+                "ogo": 98,
+                "him": 98,
+                "rad": 200,
+                "tem": 80,
+                "bio": 200,
+                "psi": 180,
+                "kro": 62
+            },
+            "cost": {
+                "rub": 1253680,
+                "may": 470,
+                "pap": 970,
+                "ves": 2190,
+                "dig": 340,
+                "qua": 410,
+                "syv": 15
+            },
+            "bonus": {
+                "end": -9,
+                "wei": 40,
+                "nvg": 1
+            }
+        },
+        {
+            "exboid": "6w0zp",
+            "id": "mas_b",
+            "class": "research",
+            "lv": "veteran",
+            "bonus": {
+                "end": -12,
+                "wei": 30,
+                "nvg": 1
+            },
+            "stats": {
+                "pul": 124,
+                "raz": 133,
+                "vzr": 190,
+                "ele": 89,
+                "ogo": 89,
+                "him": 89,
+                "rad": 200,
+                "tem": 70,
+                "bio": 180,
+                "psi": 160,
+                "kro": 60
+            },
+            "cost": {
+                "rub": 685450,
+                "may": 380,
+                "pap": 530,
+                "ves": 1600,
+                "dig": 310
+            }
+        },
+        {
+            "exboid": "96n5y",
+            "id": "mas_a",
+            "class": "research",
+            "lv": "veteran",
+            "bonus": {
+                "end": -12,
+                "wei": 25,
+                "nvg": 1
+            },
+            "stats": {
+                "pul": 112,
+                "raz": 120,
+                "vzr": 171,
+                "ele": 80,
+                "ogo": 80,
+                "him": 80,
+                "rad": 200,
+                "tem": 60,
+                "bio": 150,
+                "psi": 140,
+                "kro": 56
+            },
+            "cost": {
+                "rub": 575310,
+                "may": 203,
+                "pap": 283,
+                "ves": 850
+            }
+        },
+        {
+            "exboid": "y35jk",
+            "id": "ska_b",
+            "class": "research",
+            "lv": "master",
+            "bonus": {
+                "end": -6,
+                "wei": 10,
+                "nvg": 1
+            },
+            "stats": {
+                "pul": 130,
+                "raz": 140,
+                "vzr": 189,
+                "ele": 326,
+                "ogo": 326,
+                "him": 326,
+                "rad": 200,
+                "tem": 150,
+                "bio": 210,
+                "psi": 200,
+                "kro": 58
+            },
+            "cost": {
+                "rub": 1230890,
+                "may": 910,
+                "pap": 480,
+                "ves": 1430,
+                "dig": 560,
+                "qua": 400,
+                "syv": 15
+            }
+        },
+        {
+            "exboid": "3g69l",
+            "id": "ska_c",
+            "class": "research",
+            "lv": "master",
+            "bonus": {
+                "end": -8,
+                "wei": 10,
+                "nvg": 1
+            },
+            "stats": {
+                "pul": 130,
+                "raz": 140,
+                "vzr": 189,
+                "ele": 326,
+                "ogo": 326,
+                "him": 326,
+                "rad": 215,
+                "tem": 150,
+                "bio": 300,
+                "psi": 200,
+                "kro": 58
+            },
+            "cost": {
+                "rub": 1231750,
+                "may": 910,
+                "pap": 480,
+                "ves": 1430,
+                "dig": 560,
+                "qua": 400,
+                "syv": 15
+            }
+        },
+        {
+            "exboid": "m06k2",
+            "id": "ska_d",
+            "class": "research",
+            "lv": "master",
+            "bonus": {
+                "end": -10,
+                "wei": 10,
+                "nvg": 1
+            },
+            "stats": {
+                "pul": 142,
+                "raz": 153,
+                "vzr": 206,
+                "ele": 356,
+                "ogo": 356,
+                "him": 356,
+                "rad": 225,
+                "tem": 160,
+                "bio": 300,
+                "psi": 210,
+                "kro": 60
+            },
+            "cost": {
+                "rub": 2271140,
+                "may": 1260,
+                "pap": 880,
+                "ves": 2640,
+                "dig": 800,
+                "qua": 890,
+                "syv": 75
+            }
+        },
+        {
+            "exboid": "y3qgz",
+            "id": "koc_a",
+            "class": "combined",
+            "lv": "master",
+            "bonus": {
+                "spe": -2,
+                "end": -10,
+                "wei": 15,
+                "hea": 2.7,
+                "nvg": 1
+            },
+            "stats": {
+                "pul": 97,
+                "raz": 113,
+                "vzr": 136,
+                "ele": 652,
+                "ogo": 652,
+                "him": 652,
+                "rad": 240,
+                "tem": 240,
+                "bio": 300,
+                "psi": 225,
+                "kro": 52
+            },
+            "cost": {
+                "rub": 1244710,
+                "may": 1240,
+                "pap": 1110,
+                "ves": 1810,
+                "dig": 340,
+                "qua": 162,
+                "syv": 15
+            }
+        },
+        {
+            "exboid": "5l1yg",
+            "id": "koc_b",
+            "class": "combined",
+            "lv": "master",
+            "bonus": {
+                "spe": -2,
+                "end": -10,
+                "wei": 10,
+                "hea": 3,
+                "nvg": 1
+            },
+            "stats": {
+                "pul": 105,
+                "raz": 124,
+                "vzr": 149,
+                "ele": 711,
+                "ogo": 711,
+                "him": 711,
+                "rad": 240,
+                "tem": 240,
+                "bio": 300,
+                "psi": 240,
+                "kro": 53
+            },
+            "cost": {
+                "rub": 2193720,
+                "may": 2190,
+                "pap": 1950,
+                "ves": 3180,
+                "dig": 600,
+                "qua": 265,
+                "syv": 75
+            }
+        },
+        {
+            "exboid": "wjgp3",
+            "id": "cen_a",
+            "class": "combat",
+            "lv": "veteran",
+            "bonus": {
+                "end": -15,
+                "spe": -3,
+                "wei": 25,
+                "nvg": 1
+            },
+            "stats": {
+                "pul": 152,
+                "raz": 152,
+                "vzr": 210,
+                "ele": 59,
+                "ogo": 59,
+                "him": 59,
+                "rad": 200,
+                "tem": 30,
+                "bio": 180,
+                "psi": 180,
+                "kro": 61
+            },
+            "cost": {
+                "rub": 715010,
+                "may": 133,
+                "pap": 840,
+                "ves": 1460,
+                "dig": 390
+            }
+        },
+        {
+            "exboid": "4qlvo",
+            "id": "cen_b",
+            "class": "combat",
+            "lv": "master",
+            "bonus": {
+                "end": -15,
+                "spe": -3,
+                "wei": 25,
+                "nvg": 1
+            },
+            "stats": {
+                "pul": 167,
+                "raz": 167,
+                "vzr": 230,
+                "ele": 65,
+                "ogo": 65,
+                "him": 65,
+                "rad": 200,
+                "tem": 40,
+                "bio": 200,
+                "psi": 180,
+                "kro": 63
+            },
+            "cost": {
+                "rub": 1236380,
+                "may": 229,
+                "pap": 720,
+                "ves": 1440,
+                "dig": 450,
+                "qua": 650,
+                "syv": 15
+            }
+        },
+        {
+            "exboid": "4ql1r",
+            "id": "cen_c",
+            "class": "combat",
+            "lv": "master",
+            "bonus": {
+                "end": -15,
+                "spe": -3,
+                "wei": 25,
+                "nvg": 1
+            },
+            "stats": {
+                "pul": 182,
+                "raz": 182,
+                "vzr": 251,
+                "ele": 71,
+                "ogo": 71,
+                "him": 71,
+                "rad": 200,
+                "tem": 50,
+                "bio": 200,
+                "psi": 200,
+                "kro": 66
+            },
+            "cost": {
+                "rub": 2212890,
+                "may": 410,
+                "pap": 1290,
+                "ves": 2580,
+                "dig": 800,
+                "qua": 1160,
+                "syv": 75
+            }
+        },
+        {
+            "exboid": "96n5l",
+            "id": "leg_a",
+            "class": "combat",
+            "lv": "veteran",
+            "bonus": {
+                "end": -10,
+                "spe": -3,
+                "wei": 8,
+                "nvg": 1
+            },
+            "stats": {
+                "pul": 121,
+                "raz": 121,
+                "vzr": 168,
+                "ele": 47,
+                "ogo": 47,
+                "him": 47,
+                "rad": 200,
+                "tem": 30,
+                "bio": 150,
+                "psi": 120,
+                "kro": 56
+            },
+            "cost": {
+                "rub": 108590,
+                "acc": 109,
+                "moh": 610,
+                "gam": 139,
+                "dur": 810
+            }
+        },
+        {
+            "exboid": "wjg62",
+            "id": "leg_b",
+            "class": "combat",
+            "lv": "veteran",
+            "bonus": {
+                "end": -10,
+                "spe": -3,
+                "wei": 8,
+                "nvg": 1
+            },
+            "stats": {
+                "pul": 136,
+                "raz": 136,
+                "vzr": 189,
+                "ele": 53,
+                "ogo": 53,
+                "him": 53,
+                "rad": 200,
+                "tem": 30,
+                "bio": 150,
+                "psi": 140,
+                "kro": 59
+            },
+            "cost": {
+                "rub": 592720,
+                "may": 68,
+                "pap": 430,
+                "ves": 740
+            }
+        }
+    ],
+    "weapons": []
+}
